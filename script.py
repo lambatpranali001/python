b9639a48d0e4b9ffee4d2305de54cd2515981059
lambatpranali001 <lambatpranali001@gmail.com>
2022-12-22 07:35:45 -0500

    2212_1

---
+++ script.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+#target = "cdac.in"
+#print(target)
+
+#x="""hi ,
+#hello,
+#bye"""
+#print(x)
+
+#x='LOL'
+#print(x)
+#y="123"
+#x=y
+#print(x)
+
+#a="sanket"
+#def lol():
+#    x="budy"
+#    print(locals())
+#print(globals())
+#lol()
+#_______________________________________
+a = input("first : ")
+b = input("second : ")
+print(a)
+print(b)
+
+a,b=b,a
+
+print(a)
+print(b)
+#________________________________________
+a = input("first : ")
+b = input("second : ")
+print(a)
+print(b)
+def lol():
+  a=b
+  print(a)
+lol()
+b=a
+
+#print(a)
+print(b)
+
+
---
+++ script1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+a = int(input("first "))
+b = int(input("second "))
+c = (a,b)
+c
+
+
